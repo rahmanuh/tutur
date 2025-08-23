d8f209fcaf51946ffd00da4b0bb6af4e9983c9d3
Rahmanu Hermawan <rahmanu@pasdirumah.com>
2025-08-23 11:18:49 +0200

    Login routing for authenticating users

---
+++ app.py
@@ -0,0 +1,73 @@
+import flask
+import flask_login
+
+app = flask.Flask(__name__)
+app.secret_key = 'my super secret key'
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+# Our mock database.
+users = {'foo@bar.tld': {'password': 'secret'}}
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def load_user(email):
+    if email not in users:
+        return None
+
+    user = User()
+    user.id = email
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return None
+    user = User()
+    user.id = email
+    return user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+                <form action='login' method='POST'>
+                    <input type='text' name='user_id' id='email' placeholder='email'/>
+                    <input type='password' name='password' id='password' placeholder='password'/>
+                    <input type='submit' name='submit'/>
+                </form>
+               '''
+    email = flask.request.form.get('email')
+
+    if email in users and flask.request.form.get('password') == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized', 401
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
